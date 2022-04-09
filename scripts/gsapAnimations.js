71a9f73e33202b1c15043445d0cc685f7b7c3088
Ben Smallwood <bsmallwood@kmdig.com>
2022-04-09 17:12:47 -0400

    Update blog styles

--- scripts/gsapAnimations.js
+++ scripts/gsapAnimations.js
@@ -1,4 +1,5 @@
-window.addEventListener('DOMContentLoaded', () => {
-    gsap.from("svg path", {duration: 1, opacity: 0, stagger: 0.02});
-    gsap.from(".animate-text > *", {duration: 1, y: "100px", opacity: 0, stagger: 0.1});
+window.addEventListener('load', () => {
+    gsap.from("svg path", {duration: 1, opacity: 0, stagger: "-0.02"});
+
+    gsap.from(".animate-text > *", {duration: 1, y: "100px", opacity: 0, stagger: 0.4})
 });
